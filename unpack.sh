7274b0ce212be02cf7b254a38b0825aee7e23112
kov-serg <kovserg_rambler@rambler.ru>
2025-05-29 11:04:56 +0300

    fix unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 wget https://www.yeastar.com/download/TG200_TG400/53.18.0.46.zip
-unzip -P qazxcvvfrewsd321 53.18.0.46.zip
+unzip 53.18.0.46.zip
+unzip -P qazxcvvfrewsd321 53.18.0.46.bin
 tar -xf firmware.tar
 gcc -o make-ecc make-ecc.c
 gcc -o make-2 make-2.c
